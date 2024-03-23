026b6b2a11cc80a6bdee3a3a365c94b648a76cfb
Victor <victor.briez@epitech.eu>
2024-03-23 01:00:05 +0100

    push assets

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float mooveSpeed;
+    public Rigidbody2D rb;
+    private Vector3 velocity = Vector3.zero;
+
+    void FixedUpdate()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal") * mooveSpeed * Time.deltaTime;
+
+        MovePlayerCharacter(horizontal);
     }
 
-    // Update is called once per frame
-    void Update()
+    void MovePlayerCharacter(float _horizontal)
     {
-        
+        Vector3 targetVelocity = new Vector2(_horizontal, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
     }
 }
